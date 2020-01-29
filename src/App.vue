603026580dd285392f6c61fa8c855b084afe0380
ktgfaa <ktgfaa@naver.com>
2020-01-29 21:46:51 +0900

    vue_cli

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsData="todoItems" @removeTodo="removeTodo" @fixTodo="fixTodo"></TodoList>
+    <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+  import TodoHeader from './components/TodoHeader.vue'
+  import TodoInput from './components/TodoInput.vue'
+  import TodoList from './components/TodoList.vue'
+  import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data(){
+    return {
+      todoItems:[]
+    }
+  },
+  created() {
+    if(localStorage.length > 0) {
+      for(let i = 0;localStorage.length > i;i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    } else {
+      alert("공간을 채워주세요.");
+    }
+  },
+  methods : {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem,todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems=[];
+    },
+    removeTodo(todoItem,index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    },
+    fixTodo(index){
+    }
+  },
+  components : {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width : 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.3);
+  }
+
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn far fa-plus-square fa-2x" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            newTodoItem : ''
+        }
+    },
+    methods:{
+        addTodo(){
+            if(this.newTodoItem !== ""){
+                const value = this.newTodoItem && this.newTodoItem.trim();
+                this.$emit('addTodo',value);
+                localStorage.setItem(this.newTodoItem,this.newTodoItem);
+                // key와 value로 이루어짐
+                //trim 함수는 공백 없이 처리해줌
+                this.clearInput();
+            } else {
+                alert("공간을 채워주세요!");
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style>
+    input:focus{
+        outline : none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size:0.9rem;
+    }
+    .addContainer{
+        float : right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color : white;
+        vertical-align: middle;
+
+    }
+</style>
